6e043be573738c2ff5f823474f49429b9c78c4a4
UsikovKonstantin <usikov-kostya@mail.ru>
2023-10-07 17:24:31 +0300

    Battle function

--- Class/main.py
+++ Class/main.py
@@ -1,24 +1,29 @@
 from lib import *
 import random
 
-if __name__ == '__main__':
-    Warrior1 = Warrior(100, 20)
-    Warrior2 = Warrior(100, 20)
+
+def battle(warrior1, warrior2):
     win = 0
     while win == 0:
         n = random.randint(1, 2)
         if n == 1:
-            Warrior1.attack(Warrior2)
-            print("Warrior1 attacked Warrior2")
-            print("Warrior2 health:", Warrior2.health)
+            warrior1.attack(warrior2)
+            print("warrior1 attacked warrior2")
+            print("warrior2 health:", warrior2.health)
             print()
-            if Warrior2.health == 0:
+            if warrior2.health == 0:
                 win = 1
         elif n == 2:
-            Warrior2.attack(Warrior1)
-            print("Warrior2 attacked Warrior1")
-            print("Warrior1 health:", Warrior1.health)
+            warrior2.attack(warrior1)
+            print("warrior2 attacked warrior1")
+            print("warrior1 health:", warrior1.health)
             print()
-            if Warrior1.health == 0:
+            if warrior1.health == 0:
                 win = 2
-    print("Warrior", win, " won", sep="")
+    print("warrior", win, " won", sep="")
+
+
+if __name__ == '__main__':
+    Warrior1 = Warrior(100, 20)
+    Warrior2 = Warrior(100, 20)
+    battle(Warrior1, Warrior2)
