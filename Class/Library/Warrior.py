7e7f3af6b04a487ea68bacc95a501c0f7decd585
UsikovKonstantin <usikov-kostya@mail.ru>
2023-10-14 22:28:38 +0300

    Комментарии

---
+++ Class/Library/Warrior.py
@@ -0,0 +1,117 @@
+import random
+
+
+class Warrior:
+    """
+    Class "Warrior".
+    :param name: Warrior's name
+    :param health: Warrior's health
+    :param damage: Warrior's damage
+    """
+
+    def __init__(self, name, health, damage):
+        """
+        Class constructor.
+        :param name: Warrior's name
+        :param health: Warrior's health
+        :param damage: Warrior's damage
+        """
+        self.__name = name
+        self.__health = health
+        self.__damage = damage
+
+    @property
+    def name(self):
+        """
+        Name getter.
+        :return: Warrior's name
+        """
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        """
+        Name setter.
+        :param name: Warrior's name
+        :return: None
+        """
+        self.__name = name
+
+    @property
+    def health(self):
+        """
+        Health getter.
+        :return: Warrior's health
+        """
+        return self.__health
+
+    @health.setter
+    def health(self, health):
+        """
+        Health setter.
+        :param health: Warrior's health
+        :return: None
+        """
+        self.__health = health
+        if self.__health < 0:
+            self.__health = 0
+
+    @property
+    def damage(self):
+        """
+        Damage getter.
+        :return: Warrior's damage
+        """
+        return self.__damage
+
+    @damage.setter
+    def damage(self, damage):
+        """
+        Damage setter.
+        :param damage: Warrior's damage
+        :return: None
+        """
+        self.__damage = damage
+
+    def attack(self, enemy):
+        """
+        Method for attacking another Warrior.
+        :param enemy: Warrior to attack
+        :return: None
+        """
+        enemy.health -= self.damage
+
+    def __str__(self):
+        """
+        Convert Warrior to a string.
+        :return: Warrior's name, health and damage
+        """
+        return f"name: {self.__name}, health: {self.__health}, damage: {self.__damage}"
+
+
+def battle(warrior1, warrior2):
+    """
+    Begins a battle between two warriors.
+    :param warrior1: First Warrior
+    :param warrior2: Second Warrior
+    :return: None
+    """
+    win = 0
+    while win == 0:
+        n = random.randint(1, 2)
+        if n == 1:
+            warrior1.attack(warrior2)
+            print(f"{warrior1.name} attacked {warrior2.name}")
+            print(f"{warrior2.name}'s health:", warrior2.health, "\n")
+            if warrior2.health == 0:
+                win = 1
+        elif n == 2:
+            warrior2.attack(warrior1)
+            print(f"{warrior2.name} attacked {warrior1.name}")
+            print(f"{warrior1.name}'s health:", warrior1.health, "\n")
+            if warrior1.health == 0:
+                win = 2
+    if win == 1:
+        print(f"{warrior1.name} won")
+    elif win == 2:
+        print(f"{warrior2.name} won")
--- Class/main.py
+++ Class/main.py
@@ -1,29 +1,6 @@
-from Library.lib import *
-import random
-
-
-def battle(warrior1, warrior2):
-    win = 0
-    while win == 0:
-        n = random.randint(1, 2)
-        if n == 1:
-            warrior1.attack(warrior2)
-            print("warrior1 attacked warrior2")
-            print("warrior2 health:", warrior2.health)
-            print()
-            if warrior2.health == 0:
-                win = 1
-        elif n == 2:
-            warrior2.attack(warrior1)
-            print("warrior2 attacked warrior1")
-            print("warrior1 health:", warrior1.health)
-            print()
-            if warrior1.health == 0:
-                win = 2
-    print("warrior", win, " won", sep="")
-
+from Library.Warrior import *
 
 if __name__ == '__main__':
-    Warrior1 = Warrior(100, 40)
-    Warrior2 = Warrior(200, 20)
-    battle(Warrior1, Warrior2)
+    warrior1 = Warrior("Warrior1", 100, 20)
+    warrior2 = Warrior("Warrior2", 100, 20)
+    battle(warrior1, warrior2)
