c40a7fc7b12436a0154b647face2e7a9028adf82
UsikovKonstantin <usikov-kostya@mail.ru>
2023-10-14 13:54:52 +0300

    Обработка исключений

--- Sort/main.py
+++ Sort/main.py
@@ -3,11 +3,23 @@ from sort import *
 
 def check_sorting():
     input_lst = generate_list(10, -100, 100)
-    generate_file(input_lst, "input.txt")
-    lst = get_list_from_file("input.txt")
+    try:
+        generate_file(input_lst, "input.txt")
+    except:
+        print("Не удалось создать файл.")
+        return
+    try:
+        lst = get_list_from_file("input.txt")
+    except:
+        print("Не удалось прочитать файл.")
+        return
     print("Input: ", lst)
     sorted_list = quick_sort(lst)
-    write_list_to_file(sorted_list, "output.txt")
+    try:
+        write_list_to_file(sorted_list, "output.txt")
+    except:
+        print("Не удалось записать список в файл.")
+        return
     print("Output: ", sorted_list)
 
 
